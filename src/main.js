d44727d6d06e491131c3bcbc9484850b3d8b24a1
Vladimir <limtis2109@gmail.com>
2022-03-30 19:50:15 +0200

    Filling buttons with dates

--- src/main.js
+++ src/main.js
@@ -3,18 +3,58 @@ const months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "
 function dateFormatMonthframe(date){ return `${months[date.getMonth()]}, ${date.getDate()}`; }
 function dateFormatButtons(date){ return `${months[date.getMonth()]}\n${date.getDate()}`; }
 
-// Filling monthframe
-var date = new Date();
-date.setDate(date.getDate() - 1);
-
-// Filling monthframe
-document.getElementById("monthframe").textContent = dateFormatMonthframe(date);
-
-// Filling buttons with current dates
-var buttons = document.getElementById("calendar").getElementsByTagName("button");
-for (var i = 0; i < buttons.length; i++)
+function setMonthFrame(currentDate)
 {
-    buttons[i].textContent = dateFormatButtons(date);
+    var yesterdayDate = new Date();
+    yesterdayDate.setDate(currentDate.getDate() - 1);
+
+    var lastDate = new Date();
+    lastDate.setDate(currentDate.getDate() + 3); // Five days summary
+
+    document.getElementById("monthframe").textContent = `${dateFormatMonthframe(yesterdayDate)} - ${dateFormatMonthframe(lastDate)}`;
 }
 
+function fillButtons(currentDate)
+{
+    var date = new Date();
+    date.setDate(currentDate.getDate() - 1); // Fills from yesterday
 
+    var buttons = document.getElementById("calendar").getElementsByTagName("button");
+
+    for (var i = 0; i < buttons.length; i++)
+    {
+        buttons[i].textContent = dateFormatButtons(date);
+        date.setDate(date.getDate() + 1);
+    }
+}
+
+class CalendarItem
+{
+    constructor(eventName, temperature, iconURL, motd, backgroundURL)
+    {
+        this.eventName = eventName;
+        this.temperature = temperature;
+        this.logoURL = iconURL;
+        this.motd = motd;
+        this.backgroundURL = backgroundURL;
+    }
+}
+
+function showEvent(day)
+{
+    document.getElementById("event-name").textContent = day.eventName;
+    document.getElementById("temp").textContent = `${day.temperature}°C`;
+    document.getElementById("event-icon").src = day.logoURL;
+    document.getElementById("motd").textContent = day.motd;
+    document.body.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${day.backgroundURL})`;
+}
+
+var date = new Date();
+setMonthFrame(date);
+
+// Filling buttons with current dates
+fillButtons(date);
+
+let day = new CalendarItem("Sun explosion", 999999, "./witch.svg", "Turn the AC on", "hell.png");
+
+showEvent(day);
