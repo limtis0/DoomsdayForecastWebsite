78d888c3f84ef4bea86457730d975b197c8f5c9d
Vladimir <limtis2109@gmail.com>
2022-04-04 16:31:11 +0200

    Added week table

--- src/databases/event-list.sql
+++ src/databases/event-list.sql
@@ -14,3 +14,13 @@ CREATE TABLE events
 );
 
 --import df1.csv to database
+
+CREATE TABLE week
+(
+    timestamp date NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    temp integer NOT NULL,
+    phrase VARCHAR(255) NOT NULL,
+    icon VARCHAR(255) NOT NULL,
+    background VARCHAR(255) NOT NULL
+);
